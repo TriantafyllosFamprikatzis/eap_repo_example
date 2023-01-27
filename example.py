bdaca5eb3b7b50954f7a826d4b440256d610eb68
saradis1 <saradis_h@hotmail.com>
2023-01-27 18:27:22 +0200

    2nd edit

--- example.py
+++ example.py
@@ -2,8 +2,10 @@
 def main():
     print("Hello World!")
     print ('This is just a test from Sarantis')
+    print ('This is a second edit from Sarantis')
+
+
 
 if __name__ == "__main__":
     main()
-
-
+   
